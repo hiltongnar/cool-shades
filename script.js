644f4e6d9acf9e56c30e182a1517c4179a5634a0
Mike <macuser@macs-MacBook-Pro.local>
2019-03-26 20:23:23 -0400

    added javascript array for mobile

---
+++ script.js
@@ -0,0 +1,23 @@
+var redShades = {
+  title: "Red",
+  img: 'images/product-red.png',
+  price: "$199"
+}
+var greenShades = {
+  title: "Green",
+  img: 'images/product-green.png',
+  price: "$199"
+}
+var blueShades = {
+  title: "Blue",
+  img: 'images/product-blue.png',
+  price: "$199"
+}
+var blackShades = {
+  title: "Black",
+  img: 'images/product-black.png',
+  price: "$199"
+}
+var shades = [redShades, greenShades, blueShades, blackShades];
+
+console.log(shades);
